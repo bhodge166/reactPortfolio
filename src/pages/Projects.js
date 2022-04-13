3e73978c8bf3db9ecf35c83d512fda9aa6ee9826
Brad Hodge <bhodge166@gmail.com>
2022-04-13 16:17:36 -0600

    initial file structure

---
+++ src/pages/Projects.js
@@ -0,0 +1,11 @@
+import React from "react";
+// TODO: Add code here to import necessary functionality
+import { useParams } from "react-router-dom";
+
+import  from "../components/";
+import  from "../components/";
+
+  return (
+    
+
+export default SingleThought;
